953b15ec631c50a6df41f48af9f126b0da3929b2
HelgeKeck <helgekeck@hotmail.com>
2024-09-18 07:35:24 +0200

    Update gcode-macro-params.ts

--- src/util/gcode-macro-params.ts
+++ src/util/gcode-macro-params.ts
@@ -3,11 +3,15 @@ const defaultValueRegExp = /\|\s*default\s*\(\s*((["'])(?:\\\2|.)*?\2|-?\d[^,)]*
 
 export const gcodeMacroParamDefault = (param: string) => {
   const valueMatch = defaultValueRegExp.exec(param)
-  let result = ((valueMatch && valueMatch[1]) || '').trim()
-  if (result === "''" || result === '""') {
-    result = ''
+  if (!valueMatch) {
+    return ''
   }
-  return result
+
+  const value = (valueMatch[1] || '').trim()
+
+  return valueMatch[2]
+    ? value.substring(1, value.length - 1)
+    : value
 }
 
 const gcodeMacroParams = (gcode: string) => {
